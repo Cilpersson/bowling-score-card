f09b2da739a000845c9f26393bfbabe3bfab6785
Christina Persson <c.i.l.persson@gmail.com>
2020-09-11 11:54:14 +0200

    Second test, all one pins down. Does not work because score method has a hard coded return of 0

---
+++ src/game/bowlingGame.js
@@ -0,0 +1,7 @@
+export class BowlingGame {
+  roll() {}
+
+  score() {
+    return 0;
+  }
+}
--- src/game/bowlingGame.spec.js
+++ src/game/bowlingGame.spec.js
@@ -1,8 +1,13 @@
 import { BowlingGame } from "./bowlingGame";
 
-// I'm not completely confident in how to write the describe and it part of the test,
+// 'RED - GREEN - REFACTOR' seems like the way to go both in the java-video I watched and the
+// javascript video I'm currently watching.
+
+// I'm not completely confident in how to write the describe("") and it("") part of the test,
 // if I have understood it correctly the describe part should be understood by a non-
 // developer and the it-section shoud be a more coding technichal description?
+
+/* ALL GUTTER BALLS */
 describe("A game of all gutter balls results in 0 points", () => {
   const game = new BowlingGame();
 
@@ -10,6 +15,18 @@ describe("A game of all gutter balls results in 0 points", () => {
     for (let i = 0; i < 20; i++) {
       game.roll(0);
     }
-    expect(game.score).toEqual(0);
+    expect(game.score()).toEqual(0);
+  });
+});
+
+/* ALL ROLLS ONE PIN DOWN */
+describe("A game of all one pin down rolls should result in 20 points", () => {
+  const game = new BowlingGame();
+
+  it("should return 0 for a game of 0 points", () => {
+    for (let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20);
   });
 });
